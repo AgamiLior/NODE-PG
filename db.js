f1d0e17cdc08c33210fb4416fd68bf6222ca3dbb
Lior Agami <agamilior25@gmail.com>
2022-04-03 20:05:39 -0400

    fixing db

--- db.js
+++ db.js
@@ -5,9 +5,9 @@ const { Client } = require('pg')
 let DB_URI;
 
 if (process.env.NODE_ENV === "test") {
-    DB_URI = "postgresql://lior_agami:Pjrn39922jackS14-@localhost/biztime_test";
+    DB_URI = "postgresql:///biztime_test";
 } else {
-    DB_URI = "postgresql://lior_agami:Pjrn39922jackS14-@localhost/biztime";
+    DB_URI = "postgresql:///biztime";
 }
 
 
